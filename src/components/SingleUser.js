482535c4fc2aa41a814fd7cc900e8bd3fe7bdddc
Marianna Zurek <zurekmarianna@gmail.com>
2022-11-28 21:30:27 +0100

    styled usersPage

--- src/components/SingleUser.js
+++ src/components/SingleUser.js
@@ -8,7 +8,13 @@ const SingleUser = ({ user, onUserSelect }) => {
       className="simple-task__container"
       onClick={() => onUserSelect(user)}
     >
-      <h2 className="simple-task__title">{user.name}</h2>
+      <div className="simple-task__container-info">
+        <div className="simple-task__info_name-date">
+          <h2 className="simple-task__title">{user.name}</h2>
+          <p className="simple-task__info_date">Created {format(new Date(user.createdAt), "do MMM")}</p>
+        </div>
+        <div className="simple-task__info-img" style={{ backgroundImage: `url(${user.imageURL})`}}></div>
+      </div>
       <p className="simple-task__description">{user.description}</p>
     </article>
   );
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -30,55 +30,58 @@ const UserForm = ({
       <form onSubmit={onFormSubmit} className="new-task__container-add ">
         <div className="new-task__container">
           <div>
-          <span className="new-task__element-title">
-            <label
-              className="new-task__label-title"
-              htmlFor="new-task__input-title"
-            >
-              Name
-            </label>
-            <input
-              type="text"
-              placeholder="Type name"
-              className="new-task__input-title"
-              value={name}
-              onChange={(event) => setName(event.target.value)}
-            />
-          </span>
-          <span className="new-task__element-description">
-            <label
-              className="new-task__label-description"
-              htmlFor="new-task__input-description"
-            >
-              Description
-            </label>
-            <input
-              type="text"
-              placeholder="Type description"
-              className="new-task__input-description"
-              value={description}
-              onChange={(event) => setDescription(event.target.value)}
-            />
-          </span>
-          <span className="new-task__element-link">
-            <label
-              className="new-task__label-link"
-              htmlFor="new-task__input-link"
-            >
-              Image URL
-            </label>
-            <input
-              type="text"
-              placeholder="Type image adress"
-              className="new-task__input-link"
-              value={imageURL}
-              onChange={(event) => setImageURL(event.target.value)}
-            />
-          </span>
+            <span className="new-task__element-title">
+              <label
+                className="new-task__label-title"
+                htmlFor="new-task__input-title"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                placeholder="Type name"
+                className="new-task__input-title"
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+              />
+            </span>
+            <span className="new-task__element-description">
+              <label
+                className="new-task__label-description"
+                htmlFor="new-task__input-description"
+              >
+                Description
+              </label>
+              <input
+                type="text"
+                placeholder="Type description"
+                className="new-task__input-description"
+                value={description}
+                onChange={(event) => setDescription(event.target.value)}
+              />
+            </span>
+            <span className="new-task__element-link">
+              <label
+                className="new-task__label-link"
+                htmlFor="new-task__input-link"
+              >
+                Image URL
+              </label>
+              <input
+                type="text"
+                placeholder="Type image adress"
+                className="new-task__input-link"
+                value={imageURL}
+                onChange={(event) => setImageURL(event.target.value)}
+              />
+            </span>
           </div>
           <div className="new-task__container-user">
             <h3 className="new-task__user">Preview</h3>
-            <div className="new-user-img"></div>
+            <div
+              className="new-user-img"
+              style={{ backgroundImage: `url(${imageURL})` }}
+            ></div>
           </div>
         </div>
         <button
